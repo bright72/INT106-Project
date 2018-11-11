5b497323e7102c5059850b4c1351e4f8e46441a1
essjii <nooearn@gmail.com>
2018-11-11 16:17:25 +0700

    ปรับปรุง CartServlet เล็กน้อย

--- FarmLungRun/src/java/FLR/controller/CartServlet.java
+++ FarmLungRun/src/java/FLR/controller/CartServlet.java
@@ -5,18 +5,30 @@
  */
 package FLR.controller;
 
+import FLR.model.Account;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
 
 /**
  *
  * @author SARUNSUMETPANICH
  */
 public class CartServlet extends HttpServlet {
+    
+    @PersistenceUnit (unitName = "FarmLungRunPU")
+    EntityManagerFactory emf;
+    
+    @Resource
+    UserTransaction utx;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,7 +41,13 @@ public class CartServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("cart.jsp").forward(request, response);
+        HttpSession session = request.getSession(true);
+        Account accountSession = (Account) session.getAttribute("account");
+        if (accountSession != null) {
+            
+        } else {
+            getServletContext().getRequestDispatcher("/Login").forward(request, response);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
