6aaf191c97be293f670e8606eb85ae3c37bd0faa
essjii <nooearn@gmail.com>
2018-11-16 12:44:52 +0700

    edit CheckoutServlet

--- FarmLungRun/src/java/FLR/controller/CheckoutServlet.java
+++ FarmLungRun/src/java/FLR/controller/CheckoutServlet.java
@@ -5,11 +5,20 @@
  */
 package FLR.controller;
 
+import FLR.model.Account;
+import FLR.model.LineItem;
+import FLR.model.Orderdetail;
+import FLR.model.Orders;
 import FLR.model.Product;
 import FLR.model.ShoppingCart;
+import FLR.model.controller.AccountJpaController;
+import FLR.model.controller.OrderdetailJpaController;
+import FLR.model.controller.OrdersJpaController;
 import FLR.model.controller.ProductJpaController;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.Resource;
 import javax.persistence.EntityManagerFactory;
@@ -26,7 +35,8 @@ import javax.transaction.UserTransaction;
  * @author SARUNSUMETPANICH
  */
 public class CheckoutServlet extends HttpServlet {
-  @PersistenceUnit(unitName = "FarmLungRunPU")
+
+    @PersistenceUnit(unitName = "FarmLungRunPU")
     EntityManagerFactory emf;
 
     @Resource
@@ -44,14 +54,45 @@ public class CheckoutServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession(true);
-        ShoppingCart cart = (ShoppingCart)session.getAttribute("cart");
-        if (cart == null) {
-            request.setAttribute("message", "Your cart is empty!");
-            getServletContext().getRequestDispatcher("/checkout.jsp").forward(request, response);
+        Account accountSession = (Account) session.getAttribute("account");
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+        if (accountSession != null) {
+            if (cart == null) {
+                request.setAttribute("message", "Your cart is empty! You can't checkout anything now...");
+                getServletContext().getRequestDispatcher("/checkout.jsp").forward(request, response);
+            } else {
+                AccountJpaController accountCtrl = new AccountJpaController(utx, emf);
+                Account account = accountCtrl.findAccount(accountSession.getUsername());
+                OrdersJpaController ordersCtrl = new OrdersJpaController(utx, emf);
+                OrderdetailJpaController orderdetailCtrl = new OrderdetailJpaController(utx, emf);
+                String comment = request.getParameter("comment");
+
+                List<LineItem> lineItemList = cart.getLineItems();
+                for (LineItem lineItem : lineItemList) {
+                    Orders orders = new Orders();
+                    orders.setOrderid(orders.getOrderid() + 1);
+                    orders.setComment(comment);
+                    orders.setOrderdate(new Date());
+                    orders.setUsername(account);
+
+                    Orderdetail orderDetail = new Orderdetail();
+                    orderDetail.setOrderdetailid(orderDetail.getOrderdetailid()+1);
+                    orderDetail.setOrderid(orders);
+                    orderDetail.setProductcode(lineItem.getProduct());
+                    orderDetail.setPriceeach(lineItem.getPrice());
+                    orderDetail.setQuantity(lineItem.getQuantity());
+                    
+                    
+                }
+
+                request.setAttribute("message", "");
+                getServletContext().getRequestDispatcher("/checkout.jsp").forward(request, response);
+            }
         } else {
-            getServletContext().getRequestDispatcher("/checkout.jsp").forward(request, response);
+            request.setAttribute("message", "Your cart is empty! You can't checkout anything now...");
+            getServletContext().getRequestDispatcher("/Login").forward(request, response);
         }
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
